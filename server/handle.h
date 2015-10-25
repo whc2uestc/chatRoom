81f5132a2e2280f36cef7cbd3e24389b104c3ac1
HC.Wang <171094685@qq.com>
2015-10-25 10:44:08 +0800

    the head file of the handle.c

--- server/handle.h
+++ server/handle.h
@@ -1,8 +1,10 @@
 #ifndef HANDLE_H_
 #define HANDLE_H_
-
+#include "config.h"
 int do_login(char *name,char *ipaddr,int port,int fd);
 void do_logout(char *name,char *ipaddr,int port);
-void do_chat_public(char *name,char *message);
+void do_chat_public(UserNode *pSender,char *name,char *message);
+char* do_chat_private(char *name,char *message);
+void sub_process();
 
 #endif
